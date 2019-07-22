696e823ddd47b84826e8b1e14509ecec9c652c41
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2019-07-22 20:55:43 +0300

    Add files via upload

---
+++ NumberClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Classwork
+{
+    class NumberClass
+    {
+        public NumberClass()
+        {
+        }
+
+        public void GetNumber(string x)
+        {
+            try
+            {
+                int y = int.Parse(x);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void GetNumbers(int num1, int num2, int num3, int num4, int num5)
+        {
+            try
+            {
+                int[] array = new int[4];
+
+                array[0] = num1;
+                array[1] = num2;
+                array[2] = num3;
+                array[3] = num4;
+                array[4] = num5;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void DivideByZero(int x)
+        {
+            try
+            {
+                x /= 0;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberClass numC = new NumberClass();
+
+            Console.WriteLine("Enter a number from 1-3 to choose an option:");
+            Console.WriteLine("1. input a string into an integer");
+            Console.WriteLine("2. input 5 numbers into an array");
+            Console.WriteLine("3. divide by zero");
+
+            try
+            {
+
+
+                int option = int.Parse(Console.ReadLine());
+
+                if(option == 1)
+                numC.GetNumber("b");
+
+                if(option == 2)
+                numC.GetNumbers(1, 2, 3, 4, 5);
+
+                if(option ==3)
+                numC.DivideByZero(68);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
